119b424387b5dd2f17fc78921f68a6bed0846f59
Vladimir Pasquier <vpasquier@nuxeo.com>
2016-01-12 12:08:52 -0500

    NXP-18742: add time in audit dates

--- nuxeo-salesforce-core/src/main/java/org/nuxeo/salesforce/LogEntryEnricher.java
+++ nuxeo-salesforce-core/src/main/java/org/nuxeo/salesforce/LogEntryEnricher.java
@@ -1,15 +1,17 @@
 /*
- * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and others.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * Contributors:
  *     Vladimir Pasquier <vpasquier@nuxeo.com>
@@ -61,15 +63,19 @@ public class LogEntryEnricher extends AbstractJsonEnricher<LogEntry> {
         Date modificationDate = ((GregorianCalendar) targetDocument.getPropertyValue("dc:modified")).getTime();
 
         jsonGenerator.writeObjectFieldStart(NAME);
-        jsonGenerator.writeObjectField("contact", getPrincipalName(logEntry));
-        jsonGenerator.writeObjectField("creation", new SimpleDateFormat("yyyy-MM-dd").format(creationDate));
-        jsonGenerator.writeObjectField("modification", new SimpleDateFormat("yyyy-MM-dd").format(modificationDate));
+        jsonGenerator.writeStringField("contact", getPrincipalName(logEntry));
+        jsonGenerator.writeStringField("creation", new SimpleDateFormat("yyyy-MM-dd HH:mm a").format(creationDate));
+        jsonGenerator.writeStringField("modification", new SimpleDateFormat("yyyy-MM-dd HH:mm a").format(modificationDate));
         jsonGenerator.writeEndObject();
     }
 
     protected String getPrincipalName(LogEntry logEntry) {
         UserManager userManager = Framework.getLocalService(UserManager.class);
-        NuxeoPrincipal principal = userManager.getPrincipal(logEntry.getPrincipalName());
+        String principalName = logEntry.getPrincipalName();
+        NuxeoPrincipal principal = userManager.getPrincipal(principalName);
+        if (principal == null) {
+            return principalName;
+        }
         String firstName = principal.getFirstName();
         String lastName = principal.getLastName();
         if (Strings.isNullOrEmpty(firstName) && Strings.isNullOrEmpty(lastName)) {
